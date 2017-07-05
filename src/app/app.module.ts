ffe42851e2ff7f5672469d80e990196394b70c67
Bas van Dijk <bas@usto.nl>
2017-07-05 20:15:49 +0200

    Removed TestModule from AppModule
    https://github.com/ionic-team/ionic-app-scripts/issues/848#issuecomment-313158546

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
 
-import { TestModule } from '../test/test.module';
+// import { TestModule } from '../test/test.module';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -18,7 +18,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     ListPage
   ],
   imports: [
-    TestModule,
+    // TestModule,
     BrowserModule,
     IonicModule.forRoot(MyApp),
   ],
